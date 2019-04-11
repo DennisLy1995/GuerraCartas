0b2a1ee77da7572b1a64b691b4bdb20b2c27a4fe
RABA1390 <rafab1390@gmail.com>
2019-04-11 15:10:43 -0600

    Se añade el Main del proyeto GuerraDeCartas a GuerraDeCartasV1

--- GuerraDeCartasV1/GuerraDeCartasV1/GuerraDeCartasV1.cpp
+++ GuerraDeCartasV1/GuerraDeCartasV1/GuerraDeCartasV1.cpp
@@ -1,7 +1,43 @@
 #include "pch.h"
 #include <iostream>
 
+#include <iostream>
+using namespace std;
+
+void mostrarMenu() {
+	cout << "Por favor, seleccione una de las siguientes opciones:" << endl;
+	cout << "1. Iniciar juego." << endl;
+	cout << "2. Ver reglas del juego." << endl;
+	cout << "3. Salir. " << endl;
+}
+
+int procesarOpcion(int opcion) {
+	switch (opcion)
+	{
+	case 1:
+		cout << "Iniciando juego..." << endl; // Esto se debería de cambiar
+		return 1;
+		break;
+	case 2:
+		cout << "Reglas del juego:" << endl; // Esto se debería de cambiar
+		return 1;
+		break;
+	case 3:
+		cout << "Saliendo del juego..." << endl;
+		return -1;
+		break;
+	default:
+		return -1;
+	}
+}
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	int opc = 0;
+	while (opc != -1) {
+		mostrarMenu();
+		cin >> opc;
+		opc = procesarOpcion(opc);
+
+	}
+
 }
