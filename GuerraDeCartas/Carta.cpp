7b61700ab8512cd0ccc3cdad40da8e21053dd8b9
RABA1390 <rafab1390@gmail.com>
2019-04-11 14:56:55 -0600

    Se añaden objetos Nodo y Cartas

--- GuerraDeCartas/Carta.cpp
+++ GuerraDeCartas/Carta.cpp
@@ -1,12 +1,13 @@
 #include "Carta.h"
 
-
-
 Carta::Carta()
 {
 }
 
-
-Carta::~Carta()
+Carta::Carta(int numCarta, int valor, string simbolo, string valorSimb)
 {
+	this->numCarta = numCarta;
+	this->valor = valor;
+	this->simbolo = simbolo;
+	this->valorSimb = valorSimb;
 }
