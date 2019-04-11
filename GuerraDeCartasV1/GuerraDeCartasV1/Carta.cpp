967dac4e75a6969d76e2137ecf0ee8c5a6bf9286
RABA1390 <rafab1390@gmail.com>
2019-04-11 15:07:03 -0600

    Se añade nuevo proyecto GuerraDeCartasV1 con los objetos Carta y Nodo

---
+++ GuerraDeCartasV1/GuerraDeCartasV1/Carta.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include "Carta.h"
+
+Carta::Carta()
+{
+}
+
+Carta::Carta(int numCarta, int valor, string simbolo, string valorSimb)
+{
+	this->numCarta = numCarta;
+	this->valor = valor;
+	this->simbolo = simbolo;
+	this->valorSimb = valorSimb;
+}
